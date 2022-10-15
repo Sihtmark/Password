0fce4a30bddc2173505c165a1e3cf88543a60a44
Sergei Poluboiarinov <sihtmark@icloud.com>
2022-10-15 17:55:35 +0300

    Initial Commit

---
+++ Password/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Password
+//
+//  Created by Sergei Poluboiarinov on 15.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
